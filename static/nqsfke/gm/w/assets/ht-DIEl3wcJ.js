bfb7522bef29c7305e3765501d1cf63006f7f8df
lvan8998 <94165584@qq.com>
2026-01-17 10:30:30 +0800

    Update ht-DIEl3wcJ.js
    分类#

--- static/nqsfke/gm/w/assets/ht-DIEl3wcJ.js
+++ static/nqsfke/gm/w/assets/ht-DIEl3wcJ.js
@@ -741,7 +741,7 @@ const Ae = "arttmpl",
 		},
 		component: () => $(() => import("./chunk-nKNq4phu.js"), __vite__mapDeps([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]))
 	},{
-		path: "#/type/:category/:subCategory?",
+		path: "/#/type/:category/:subCategory?",
 		name: "TypePage",
 		meta: {
 			title: "分类"

f268e7f517440dce1c506b22ddd2c798463ca6c6
lvan8998 <94165584@qq.com>
2026-01-18 23:51:30 +0800

    Update ht-DIEl3wcJ.js

--- static/nqsfke/gm/w/assets/ht-DIEl3wcJ.js
+++ static/nqsfke/gm/w/assets/ht-DIEl3wcJ.js
@@ -734,8 +734,8 @@ const Ae = "arttmpl",
 		},
 		component: () => $(() => import("./chunk-nKNq4phu.js"), __vite__mapDeps([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]))
 	}, {
-		path: "/BoxOS/static/#/",
-		name: "Home",
+		path: "/BoxOS/static/#",
+		name: "Home3",
 		meta: {
 			title: "首页2"
 		},
@@ -743,7 +743,7 @@ const Ae = "arttmpl",
 	},
 {
 		path: "/BoxOS/static/",
-		name: "Home2",
+		name: "Home",
 		meta: {
 			title: "首页3"
 		},

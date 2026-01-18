8151db82b6d45036781d5a47e59cc8d8ca8325b5
lvan8998 <94165584@qq.com>
2026-01-19 00:50:52 +0800

    Update ht-DIEl3wcJ.js

--- static/nqsfke/gm/w/assets/ht-DIEl3wcJ.js
+++ static/nqsfke/gm/w/assets/ht-DIEl3wcJ.js
@@ -733,8 +733,7 @@ const Ae = "arttmpl",
 			title: "首页"
 		},
 		component: () => $(() => import("./chunk-nKNq4phu.js"), __vite__mapDeps([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]))
-	}, 
-		  {
+	},  {
 		path: "/type/:category/:subCategory?",
 		name: "TypePage",
 		meta: {

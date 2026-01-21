96112c2bd7ca3942719432c656a8a50688f90fe4
lvan8998 <94165584@qq.com>
2026-01-22 01:37:46 +0800

    Update ht-DIEl3wcJ.js

--- static/nqsfke/gm/w/assets/ht-DIEl3wcJ.js
+++ static/nqsfke/gm/w/assets/ht-DIEl3wcJ.js
@@ -774,7 +774,7 @@ const Ae = "arttmpl",
     },
     component: () => {
         console.log('开始加载Detail组件，ID:', Pe.currentRoute.value?.params?.id);
-        return $(() => import("./chunk-CUOO64VY.js").then(e => {
+        return $(() => import("/chunk-CUOO64VY.js").then(e => {
             console.log('Detail组件加载完成');
             return e.i;
         }), __vite__mapDeps([29, 6, 3, 5, 2, 26, 4, 30, 7, 31, 32, 10, 33, 34, 12, 13, 14]));
